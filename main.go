6251191556f6e3883b040ac39373c3fccb259dc0
Pierre-Louis Serré <plouiserre@gmail.com>
2021-12-27 19:32:49 +0100

    continue helloworld feature

--- main.go
+++ main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Message struct {
+	Greeting string `json:"Greeting"`
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world!!!!!")
 	fmt.Println("Endpoint Hit: homepage")
 }
 
+func sayHelloWorldFrench(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: french Page")
+	hiFrench := Message{
+		Greeting: "Salut tout le monde!!!",
+	}
+	json.NewEncoder(w).Encode(hiFrench)
+}
+
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/french", sayHelloWorldFrench)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
