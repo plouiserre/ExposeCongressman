82454aeedfcc87e965ba46db9d6daa1b814d7eee
Pierre-Louis Serré <plouiserre@gmail.com>
2022-01-22 19:45:33 +0100

    mandaterepository code coded

---
+++ Models/mandateModel.go
@@ -0,0 +1,21 @@
+package Models
+
+import "database/sql"
+
+type MandateModel struct {
+	Id              int64          `json:"Id"`
+	Uid             string         `json:"Uid"`
+	TermOffice      int64          `json:"TermOffice"`
+	TypeOrgane      string         `json:"TypeOrgane"`
+	StartDate       string         `json:"StartDate"`
+	EndDate         sql.NullString `json:"EndDate"`
+	Precedence      string         `json:"Precedence"`
+	PrincipleNaming string         `json:"PrincipleNaming"`
+	QualityCode     string         `json:"QualityCode"`
+	QualityLabel    string         `json:"QualityLabel"`
+	QualityLabelSex string         `json:"QualityLabelSex"`
+	RefBody         sql.NullString `json:"RefBody"`
+	CongressmanId   string         `json:"CongressmanId"`
+}
+
+type MandatesModel []MandateModel
